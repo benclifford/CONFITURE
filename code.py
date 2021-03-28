886fea4cd58df5140fbb29aea6d9c89b2a6bca7b
Ben Clifford <benc@hawaga.org.uk>
2021-03-28 16:49:56 +0000

    Make code.py importable rather than taking over when it is imported

--- code.py
+++ code.py
@@ -5,23 +5,25 @@ import jar
 
 print("CONFITURE: jam jar executive")
 
-current_mode = jar.primaryswitcher1()
-current_mode_pos = 0
-
 rotary_encoder = rotaryio.IncrementalEncoder(board.GP3, board.GP4)
-
-n = 0
 
 modes = [jar.primaryswitcher1, jar.phase4, jar.primaryswitcher2, jar.contr2, jar.phase1, jar.rgb1]
 
-while True:
-  print(f"driving, iteration {n}, encoder is {rotary_encoder.position}")
+def main_driver():
+  n = 0
+  current_mode = jar.primaryswitcher1()
+  current_mode_pos = 0
+  while True:
+    # print(f"driving, iteration {n}, encoder is {rotary_encoder.position}")
 
-  if rotary_encoder.position != current_mode_pos:
+    if rotary_encoder.position != current_mode_pos:
       print("Changing mode")
       current_mode_pos = rotary_encoder.position
       new_mode = current_mode_pos % len(modes)
       current_mode = modes[new_mode]()
 
-  current_mode.__next__()
-  n += 1
+    current_mode.__next__()
+    n += 1
+
+if __name__ == "__main__":
+  main_driver()
