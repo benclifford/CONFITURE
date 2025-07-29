6f811ba1df0ec016a1dd1ddd86ca8ff99d445dc5
Ben Clifford <benc@hawaga.org.uk>
2025-07-29 12:33:24 +0000

    configuration as actually used on mirella's board for the last few years - different pin, and one less LED due to breakage

--- configuration.py
+++ configuration.py
@@ -3,5 +3,5 @@ import board
 # First version of board, neopixels on pin GP13, 49 pixels
 # Second version of board, neopixels on pin GP18, 50 pixels
 
-neopixel_pin = board.GP18
-num_leds = 50
+neopixel_pin = board.GP13
+num_leds = 49
